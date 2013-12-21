0d4b4b7232115716644f3aa3e5b35b32881579e9
Chris <chris.haack@me.com>
2013-12-21 08:51:20 -0500

    create rabbit app with Datamapper ORM

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'data_mapper'
+gem 'sqlite3'
+gem 'dm-sqlite-adapter'
+gem 'haml'
+
+group :development do
+	require 'shotgun'
+	require 'debugger'
+	require 'ap'
+end
---
+++ app.rb
@@ -0,0 +1,66 @@
+require 'bundler'
+Bundler.require
+require_relative './lib/rabbit'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/rabbits.db")
+
+class App < Sinatra::Application
+
+	#list all rabbits
+	get '/rabbits' do
+		@rabbits = Rabbit.all
+		haml :index
+	end
+
+	#add new rabbit
+	get '/rabbits/new' do
+		@rabbit = Rabbit.new
+		haml :new
+	end
+
+	#edit rabbit
+	post '/rabbits' do
+		@rabbit = Rabbit.new(params[:rabbit])
+		if @rabbit.save
+			status 201
+			redirect '/rabbits' + @rabbit.id.to_s
+		else
+			status 400
+			haml :new
+		end
+	end
+
+	#update rabbit handler/route
+	put '/rabbits/:id' do
+		@rabbit = Rabbit.get(params[:id])
+		if @rabbit.update(params[:rabbit])
+			status 201
+			redirect '/rabbits/' + params[:id]
+		else
+			status 400
+			haml :edit
+		end
+	end
+
+	#delete rabbit confirmation handler
+	get '/rabbits/delete/:id' do
+		@rabbit = Rabbit.get(params[:id])
+		haml :delete
+	end
+
+	#delete rabbit handler
+	delete 'rabbits/:id' do
+		Rabbit.get(params[:id]).destroy
+		redirect '/rabbits'
+	end
+
+	#show rabbit
+	get '/rabbits/:id' do
+		@rabbit = Rabbit.get(params[:id])
+		haml :show
+	end
+
+end
+
+
+DataMapper.auto_upgrade!
