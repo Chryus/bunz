c90b45bd7c6e7e8fd6eb3d5b69d49753448bbb6a
Chris <chris.haack@me.com>
2013-12-25 11:00:22 -0800

    set explicit config in app.rb

--- bunz_active_record/app.rb
+++ bunz_active_record/app.rb
@@ -5,4 +5,11 @@ set :database, "sqlite3:///rabbit.db"
 
 class Rabbit < ActiveRecord::Base
 	validates :name, :uniqueness => true, :presense => true
+
+	#this is just explicit stuff so Sinatra knows this is the root http://ashleygwilliams.github.io/ratpack/
+  configure do
+    set :root, File.dirname(__FILE__)
+    set :views, Proc.new { File.join(root, "views") }
+  end
+  
 end
